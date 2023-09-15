11d031f963888716e79b94c59d120ffff77c7ee2
yuchaosu <su.yuchao@outlook.com>
2023-09-14 21:13:34 -0400

    Adapte relu sample to akita3.0.0

--- samples/relu/main.go
+++ samples/relu/main.go
@@ -2,24 +2,67 @@ package main
 
 import (
 	_ "embed"
-	"strings"
+	"fmt"
 
 	"github.com/sarchlab/akita/v3/sim"
+	"github.com/sarchlab/zeonica/api"
+	"github.com/sarchlab/zeonica/cgra"
 	"github.com/sarchlab/zeonica/config"
 	"github.com/tebeka/atexit"
 )
 
+var width = 4
+var height = 4
+
 //go:embed relu.cgraasm
 var program string
+
+func relu(driver api.Driver) {
+	length := 10
+
+	src := make([]uint32, length)
+	dst := make([]uint32, length)
+
+	for i := 0; i < length; i++ {
+		src[i] = uint32(i)
+	}
+
+	driver.FeedIn(src, cgra.West, [2]int{0, height}, height)
+	driver.Collect(dst, cgra.East, [2]int{0, height}, height)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			driver.MapProgram(program, [2]int{x, y})
+		}
+	}
+
+	driver.Run()
+
+	fmt.Println(src)
+	fmt.Println(dst)
+
+}
 
 func main() {
 	engine := sim.NewSerialEngine()
 
-	device := config.CreateDevice(engine)
-	device.Tiles[0].Core.Code = strings.Split(program, "\n")
+	driver := api.DriverBuilder{}.
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		Build("Driver")
 
-	device.Tiles[0].Core.TickLater(0)
+	device := config.DeviceBuilder{}.
+		WithEngine(engine).
+		WithFreq(1 * sim.GHz).
+		WithWidth(width).
+		WithHeight(height).
+		Build("Device")
 
-	engine.Run()
+	//device.Tiles[0].Core.Code = strings.Split(program, "\n")
+	//device.Tiles[0].Core.TickLater(0)
+
+	//engine.Run()
+	driver.RegisterDevice(device)
+	relu(driver)
 	atexit.Exit(0)
 }
