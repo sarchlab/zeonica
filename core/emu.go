b19d786eec739ce271129e0d1db808c92c4321ff
n0thingNoob <yshi16@wm.edu>
2026-05-07 22:13:40 +0000

    Move opcode dispatch maps

--- core/emu.go
+++ core/emu.go
@@ -1848,86 +1848,9 @@ func (i instEmulator) RunOperation(inst Operation, state *coreState, time float6
 	// 	i.runAlwaysSendRec(tokens, state)
 	// }
 
-	instFuncs := map[string]func(Operation, *coreState) map[Operand]cgra.Data{
-		"ADD":       i.runAdd, // ADD, ADDI, INC, SUB, DEC
-		"SUB":       i.runSub,
-		"SHL":       i.runSHL,
-		"LRS":       i.runLRS,
-		"MUL":       i.runMul,    // MULI
-		"MUL_ADD":   i.runMulAdd, // dst = src0 * src1 + src2
-		"DIV":       i.runDiv,
-		"OR":        i.runOR,
-		"XOR":       i.runXOR, // XOR XORI
-		"AND":       i.runAND,
-		"MOV":       i.runMov,
-		"JMP":       i.runJmp,
-		"BNE":       i.runBne,
-		"BEQ":       i.runBeq, // BEQI
-		"BLT":       i.runBlt,
-		"PHI_CONST": i.runPhiConst, // backward compatibility
-		"SEXT":      i.runMov,      // identity operation by now
-		"ZEXT":      i.runMov,      // identity operation by now
-
-		"FADD": i.runFAdd, // FADDI
-		"FSUB": i.runFSub,
-		"FMUL": i.runFMul,
-		"NOP":  i.runNOP,
-
-		"PHI":             i.runPhi,
-		"PHI_START":       i.runPhiStart,
-		"GRANT_PREDICATE": i.runGrantPred,
-		"GRANT_ONCE":      i.runGrantOnce,
-		"SEL":             i.runSel,
-
-		// comparisons
-		"ICMP_EQ":  i.runCmpExport,
-		"ICMP_SLT": i.runLTExport,
-		"ICMP_SGT": i.runGTExport,
-		"ICMP_SGE": i.runSGEExport,
-		"ICMP_ULT": i.runULTExport,
-
-		// do not distinguish between data_mov and control mov
-		"DATA_MOV": i.runMov,
-		"CTRL_MOV": i.runMov,
-		"CONSTANT": i.runMov,
-
-		"GEP": i.runGep,
-
-		"CMP_EXPORT": i.runCmpExport,
-
-		"LT_EX": i.runLTExport,
-
-		"LOAD":  i.runLoadDirect,
-		"STORE": i.runStoreDirect,
-		"LDD":   i.runLoadDirect,  // backward compatibility
-		"STD":   i.runStoreDirect, // backward compatibility
-
-		"LD":  i.runLoadDRAM,
-		"LDW": i.runLoadWaitDRAM,
-
-		"ST":  i.runStoreDRAM,
-		"STW": i.runStoreWaitDRAM,
-
-		"TRIGGER": i.runTrigger,
-
-		"NOT": i.runNot,
-	}
-
-	vectorFuncs := map[string]func(Operation, *coreState) map[Operand]cgra.Data{
-		"VBROADCAST":        i.runVBroadcast,
-		"VADD":              i.runVAdd,
-		"VMUL":              i.runVMul,
-		"VECTOR.REDUCE.ADD": i.runVectorReduceAdd,
-		"VEXTRACT":          i.runVExtract,
-		"VLOAD_CONTIG":      i.runVLoadContig,
-		"VSTORE_CONTIG":     i.runVStoreContig,
-	}
-
-	retFuncs := map[string]func(Operation, *coreState, float64) map[Operand]cgra.Data{
-		"RETURN_VALUE": i.runRetImm,
-		"RETURN_VOID":  i.runRetDelay,
-		"RET":          i.runRetImm, // backward compatibility
-	}
+	instFuncs := i.scalarOpcodeFuncs()
+	vectorFuncs := i.vectorOpcodeFuncs()
+	retFuncs := i.returnOpcodeFuncs()
 
 	if instFunc, ok := vectorFuncs[instName]; ok {
 		return instFunc(inst, state)
